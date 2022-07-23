717297416a6e6457f634a55f99ad74e5b4d219fb
Rafael Kolbe Mendes <rafa.kolbe@hotmail.com>
2022-07-23 17:54:02 -0300

    ...

--- data/monsters.js
+++ data/monsters.js
@@ -12,7 +12,7 @@ const monsterFactory = (name, level, expGain, hp, atk) => {
 };
 
 const rat = monsterFactory('Rat', 1, 100, [20, 20], 3);
-const spider = monsterFactory('Spider', 2, 3000, [16, 16], 5);
+const spider = monsterFactory('Spider', 2, 3000, [26, 26], 5);
 
 const monster = {
     rat,
